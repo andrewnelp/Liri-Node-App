920984dc7e7fad932cde18eb7cc97ca7b2014127
Andrey Nelepov <nelepow@gmail.com>
2019-05-11 19:59:55 -0400

    template is added

--- liri.js
+++ liri.js
@@ -1,4 +1,41 @@
 require("dotenv").config();
 const keys = require("./keys.js");
+let inquirer = require("inquirer");
+let moment = require("moment")
+// let spotify = new Spotify(keys.spotify);
 
-let spotify = new Spotify(keys.spotify);
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    {
+      type: "list",
+      message: "What would you like to ask about: concerts, spotify songs, movies?",
+      choices: ["concert-this", "spotify-this-song", "movie-this"],
+      name: "choice"
+    },
+  ])
+  .then(function (res) {
+    if (res.choice === "concert-this" ){
+      console.log("\n=================");
+      console.log("\nWelcome " + res.username);
+      console.log(res.choice);
+      console.log("\n=================");
+    } else if (res.choice === "spotify-this-song") {
+      console.log("\n=================");
+      console.log("\nWelcome " + res.username);
+      console.log(res.choice);
+      console.log("\n=================");
+    } else if (res.choice === "movie-this") {
+      console.log("\n=================");
+      console.log("\nWelcome " + res.username);
+      console.log(res.choice);
+      console.log("\n=================");
+    }
+    
+    
+  }) 
