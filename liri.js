7bcada4833008b0f505f86686e295c14b69ed92b
Andrey Nelepov <nelepow@gmail.com>
2019-05-12 12:41:12 -0400

    finished with moment

--- liri.js
+++ liri.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const keys = require("./keys.js");
 let inquirer = require("inquirer");
 let moment = require("moment");
+moment().format();
 let axios = require("axios");
-// let spotify = new Spotify(keys.spotify);
-
-
-inquirer
-  .prompt([
+var Spotify = require('node-spotify-api');
+let spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify({
+//   id: 40dc70b0ddd94326b30d458d87bb0ffc,
+//   secret: c2f7e0f876834aefa76afe4fafab4af2
+// });
+      
+      //calling the name of the user
+      inquirer
+        .prompt([
     {
       type: "input",
       message: "What is your name?",
@@ -20,22 +26,67 @@ inquirer
       name: "choice"
     },
   ])
-  .then(function(res) {
-    if (res.choice === "concert-this" ){
+  //concert this
+  .then(function (res) {
+    if (res.choice === "concert-this") {
       console.log("\n=================");
       console.log("\nWelcome " + res.username);
-      console.log(res.choice);
       console.log("\n=================");
+      //asking about an artist
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What artist are you interested?",
+            name: "artist"
+          }
+        ]).then(function (result) {
+          let artist = result.artist;
+          //calling Bands in Town api
+          let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+          //getting results from API
+          axios.get(queryUrl).then(
+            function (response) {
+              //looping through resonse data info
+              for (let i = 0; i < response.data.length; i++) {
+                //converting date with moment()
+                let date = moment(response.data[i].datetime).format('MM/DD/YYYY')
+                console.log("\n=================");
+                console.log("Venue name: " + response.data[i].venue.name);
+                console.log("Country: " + response.data[i].venue.country);
+                console.log("Date: " + date);
+                console.log("=================");
+              }
+            })
+        })
     } else if (res.choice === "spotify-this-song") {
       console.log("\n=================");
       console.log("\nWelcome " + res.username);
-      console.log(res.choice);
       console.log("\n=================");
+      //asking about movie
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What track are you interested?",
+            name: "track"
+          }
+        ])
+        .then(function (result) {
+          spotify.search({ type: 'track', query: result.track }, function (err, data) {
+            if (err) {
+              console.log('Error occurred: ' + err);
+              return;
+            }
+            console.log(result.data)
+          });
+        })
+      // OMDB movie-this
     } else if (res.choice === "movie-this") {
       console.log("\n=================");
       console.log("\nWelcome " + res.username);
-      console.log(res.choice);
       console.log("\n=================");
+      //asking about movie
       inquirer
         .prompt([
           {
@@ -44,39 +95,45 @@ inquirer
             name: "movie"
           }
         ]).then(function (result) {
-          console.log(result.movie);
-      axios.get("http://www.omdbapi.com/?t=" + result.movie + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-          if(result.movie) {
-            console.log(response.data.Year);
-          } else  if (undefined) {
-            console.log("No such movie " + result.movie +" in our database! Try Again")
+          //if a user enters nothing then Mr Nobody
+          if (result.movie == "") {
+            axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then(
+              function (response) {
+                  console.log("\n=================");
+                  console.log("Title: " + response.data.Title);
+                  console.log("Year: " + response.data.Year);
+                  console.log("IMBD Rating: " + response.data.imdbRating);
+                  console.log("Country: " + response.data.Country);
+                  console.log("Language: " + response.data.Language);
+                  console.log("Actors: " + response.data.Actors);
+                  console.log("Plot: " + response.data.Plot);
+                  console.log("\n=================");
+              })
+          } 
+          else {
+            axios.get("http://www.omdbapi.com/?t=" + result.movie + "&y=&plot=short&apikey=trilogy").then(
+              function (response) {
+                // if Error
+                if (response.data.Error) {
+                  console.log('Movie not found!');
+                }
+                //if movie found
+                else if (result.movie) {
+                  console.log("\n=================");
+                  console.log("Title: " + response.data.Title);
+                  console.log("Year: " + response.data.Year);
+                  console.log("IMBD Rating: " + response.data.imdbRating);
+                  console.log("Country: " + response.data.Country);
+                  console.log("Language: " + response.data.Language);
+                  console.log("Actors: " + response.data.Actors);
+                  console.log("Plot: " + response.data.Plot);
+                  console.log("\n=================");
+                }
+              })
           }
-          
-        })
-        .catch(function (error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-      })  
-  }
- })
-    
 
-  
+        })
+    }
+  })
+
+
