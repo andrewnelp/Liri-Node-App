2d916f0c9736d06675f7fa87621409c01b11ffc5
Andrey Nelepov <nelepow@gmail.com>
2019-05-11 20:55:45 -0400

    movie this is added good

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 let inquirer = require("inquirer");
-let moment = require("moment")
+let moment = require("moment");
+let axios = require("axios");
 // let spotify = new Spotify(keys.spotify);
 
 
@@ -19,7 +20,7 @@ inquirer
       name: "choice"
     },
   ])
-  .then(function (res) {
+  .then(function(res) {
     if (res.choice === "concert-this" ){
       console.log("\n=================");
       console.log("\nWelcome " + res.username);
@@ -35,7 +36,47 @@ inquirer
       console.log("\nWelcome " + res.username);
       console.log(res.choice);
       console.log("\n=================");
-    }
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What movie are you interested?",
+            name: "movie"
+          }
+        ]).then(function (result) {
+          console.log(result.movie);
+      axios.get("http://www.omdbapi.com/?t=" + result.movie + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+          if(result.movie) {
+            console.log(response.data.Year);
+          } else  if (undefined) {
+            console.log("No such movie " + result.movie +" in our database! Try Again")
+          }
+          
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+      })  
+  }
+ })
     
-    
-  }) 
+
+  
